b3430ae83d574dfe0a1a25847853d49b66126c14
ksa237 <ksa237@yandex.ru>
2025-05-07 20:14:50 +1000

    correction hw

--- hw1_5/src/Book.java
+++ hw1_5/src/Book.java
@@ -1,6 +1,6 @@
 public class Book {
 
-    public static int PP = 3;
+    public static int priceOfPgs = 3;
 
     public String title;
     public int releaseYear;
@@ -8,43 +8,36 @@ public class Book {
     public int pages;
 
     public Book(String title, int releaseYear, String author, int pages) {
-        this.title=title;
-        this.releaseYear=releaseYear;
-        this.author=author;
-        this.pages=pages;
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.author = author;
+        this.pages = pages;
     }
 
     @Override
     public String toString() {
-        return "Книга /" + title + "/ написана в " + releaseYear + " г., в кол-ве страниц: "+ pages + ", автор: " + author;
+        return "Книга /" + title + "/ написана в " + releaseYear + " г., в кол-ве страниц: " + pages + ", автор: " + author;
     }
 
     public boolean isBig(int quantity) {
-        if (quantity > 500) {
-            return true;
-        } else {
-            return false;
-        }
+        return quantity > 500;
     }
 
     public boolean matches(String find) {
 
-        if (title.contains(find) == true) {
+        if (title.contains(find)) {
             return true;
 
-        } else if (author.contains(find) == true) {
+        } else if (author.contains(find)) {
             return true;
-
         } else {
             return false;
-
         }
 
     }
 
     public int estimatePrice() {
-        int amount = pages * PP;
-
+        int amount = pages * priceOfPgs;
 
         if (amount < 250) {
             return 250;
